4220c0eb52db410b321130381ef275bb2efb28de
Igor' Khan <igor.swit4@gmail.com>
2019-01-06 22:58:19 +1000

    some changes in gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var bs = require('browser-sync').create();
-//var scss = require("gulp-scss");
 var sass = require('gulp-sass');
 var notify = require("gulp-notify");
 var plumber = require("gulp-plumber");
@@ -9,27 +8,25 @@ var sourcemaps = require("gulp-sourcemaps");
 var pug = require("gulp-pug");
 var del = require("del");
 var rs = require("run-sequence");
-//var bemto = require('gulp-bemto');
-
 
 gulp.task( 'server', ['scss', 'pug', 'copy:js', 'copy:libs', 'copy:img', 'copy:img2', 'copy:fonts'], function(){
     bs.init({
         server: {
-            baseDir: './build/'
+            baseDir: 'build/'
         }
     });
     
-    gulp.watch( './src/pug/**/*.*', ['pug'] );
-    gulp.watch( './src/assets/scss/**/*.scss', ['scss'] );
-    gulp.watch( './src/assets/js/**/*.js', ['copy:js'] );
-    gulp.watch( './src/assets/libs/**/*.*', ['copy:libs'] );
-    gulp.watch( './src/assets/img/**/*.*', ['copy:img'] );
-    gulp.watch( './src/images/**/*.*', ['copy:img2'] );
-    gulp.watch( './src/assets/fonts/**/*.*', ['copy:fonts'] );
-    gulp.watch( './src/assets/js/**/*.js').on('change', bs.reload );
+    gulp.watch( 'src/pug/**/*.*', ['pug'] );
+    gulp.watch( 'src/assets/scss/**/*.scss', ['scss'] );
+    gulp.watch( 'src/assets/js/**/*.js', ['copy:js'] );
+    gulp.watch( 'src/assets/libs/**/*.*', ['copy:libs'] );
+    gulp.watch( 'src/assets/img/**/*.*', ['copy:img'] );
+    gulp.watch( 'src/images/**/*.*', ['copy:img2'] );
+    gulp.watch( 'src/assets/fonts/**/*.*', ['copy:fonts'] );
+    gulp.watch( 'src/assets/js/**/*.js').on('change', bs.reload );
 });
 gulp.task('scss', function () {
-    return gulp.src('./src/assets/scss/style.scss')
+    return gulp.src('src/assets/scss/style.scss')
         .pipe(plumber({
             errorHandler: notify.onError(function(err){
                 return {
@@ -39,18 +36,17 @@ gulp.task('scss', function () {
             })
         }))
         .pipe( sourcemaps.init() )
-        //.pipe( scss() )
         .pipe( sass() )
         .pipe( prefixer({
             browsers: ['last 5 versions'],
             cascade: false
         }) )
         .pipe( sourcemaps.write() )        
-        .pipe( gulp.dest('./build/assets/css') )
+        .pipe( gulp.dest('build/assets/css') )
         .pipe( bs.stream() );
 });
 gulp.task('pug', function(){
-    return gulp.src('./src/pug/*.pug')
+    return gulp.src('src/pug/*.pug')
         .pipe(plumber({
             errorHandler: notify.onError(function(err){
                 return {
@@ -59,41 +55,40 @@ gulp.task('pug', function(){
                 }
             })
         }))
-        //.pipe(bemto())
         .pipe( pug({
             pretty: true
         }) )
-        .pipe( gulp.dest('./build') )
+        .pipe( gulp.dest('build') )
         .pipe( bs.stream() );
 });
 gulp.task('copy:js', function(){
-    return gulp.src('./src/assets/js/**/*.*')
-        .pipe( gulp.dest('./build/assets/js') )
+    return gulp.src('src/assets/js/**/*.*')
+        .pipe( gulp.dest('build/assets/js') )
         .pipe( bs.stream() );
     
 });
 gulp.task('copy:libs', function(){
-    return gulp.src('./src/assets/libs/**/*.*')
-        .pipe( gulp.dest('./build/assets/libs') )
+    return gulp.src('src/assets/libs/**/*.*')
+        .pipe( gulp.dest('build/assets/libs') )
         .pipe( bs.stream() );
 });
 gulp.task('copy:img', function(){
-    return gulp.src('./src/assets/img/**/*.*')
-        .pipe( gulp.dest('./build/assets/img') )
+    return gulp.src('src/assets/img/**/*.*')
+        .pipe( gulp.dest('build/assets/img') )
         .pipe( bs.stream() );
 });
 gulp.task('copy:img2', function(){
-    return gulp.src('./src/images/**/*.*')
-        .pipe( gulp.dest('./build/images') )
+    return gulp.src('src/images/**/*.*')
+        .pipe( gulp.dest('build/images') )
         .pipe( bs.stream() );
 });
 gulp.task('copy:fonts', function(){
-    return gulp.src('./src/assets/fonts/**/*.*')
-        .pipe( gulp.dest('./build/assets/fonts') )
+    return gulp.src('src/assets/fonts/**/*.*')
+        .pipe( gulp.dest('build/assets/fonts') )
         .pipe( bs.stream() );
 });
 gulp.task('clean:build', function(){
-    return del('./build');
+    return del('build');
 });
 gulp.task( 'default', function(callback){
     rs(
@@ -102,5 +97,3 @@ gulp.task( 'default', function(callback){
         callback
     );
 } );
-
-// gulp.task('name', ['tasks'], function(){ });
